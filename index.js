046db0efd125ac2ef178a112b573b33e07db5eed
Julia Cooke <juliariec@gmail.com>
2021-05-13 17:02:33 -0400

    Retrieves posts to publish with relevant properties

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 require("dotenv").config();
 const { Client } = require("@notionhq/client");
 
-// Initialize Notion client
+// Initializes Notion client
 const notion = new Client({
   auth: process.env.NOTION_TOKEN,
 });
 
-getPostsToPublish();
+main();
 
+async function main() {
+  let posts = getPostsToPublish();
+  for (const post in posts) {
+    await convertPostToMarkdown(post);
+  }
+}
+
+/**
+ * Get posts to publish, including all relevant properties.
+ */
 async function getPostsToPublish() {
   const databaseId = process.env.DATABASE_ID;
+  // Pulls the posts in my blog database checked Publish
   const response = await notion.databases.query({
     database_id: databaseId,
     filter: {
@@ -19,9 +30,36 @@ async function getPostsToPublish() {
       },
     },
   });
-  const posts = response.results;
+  const full_posts = response.results;
 
-  for (const post of posts) {
-    console.log(post.properties.Name.title[0].plain_text);
+  let posts = [];
+
+  // Pulls relevant properties of the posts
+  for (const post of full_posts) {
+    posts.push({
+      name: post.properties.Name.title[0].plain_text,
+      id: post.id,
+      published: post.last_edited_time,
+      description: post.properties.Description.rich_text[0].plain_text,
+      slug: post.properties.Slug.rich_text[0].plain_text,
+      category: post.properties.Category.select.name,
+      tags:
+        post.properties.Tags.multi_select.length > 1
+          ? post.properties.Tags.multi_select.map((tag) => tag.name)
+          : [],
+    });
   }
+
+  return posts;
+}
+
+/**
+ * Converts post in Notion into Markdown format.
+ * @param {Object} post Post to convert to Markdown.
+ */
+async function convertPostToMarkdown(post) {
+  const page_blocks = await notion.blocks.children.list({
+    block_id: post.id,
+  });
+  //console.log(page_blocks);
 }
